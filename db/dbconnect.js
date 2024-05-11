770b6989e5b2e78f188c95719cce3c6bf874917d
Arpit <omagrahari55@gmail.com>
2024-05-12 02:30:54 +0530

    mongo db url

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const dbconnect=()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017",({dbName:"todobackend"})).then(()=>{
+    mongoose.connect(process.env.Mongo_URL,({dbName:"todobackend"})).then(()=>{
         console.log("Db is connected");
     }).catch((e)=>{
         console.log(e);
