7898e6d8721091de388a88b1a1579366ede58d0d
Arpit <omagrahari55@gmail.com>
2024-05-23 13:41:18 +0530

    cookies makage increase

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,13 +1,13 @@
-import jwt from "jsonwebtoken"
-export const sendcookies=(user,res,message)=>{
-    const token= jwt.sign({_id: user._id},process.env.jwt_secret);
-    res.cookie("token",token,{
+import jwt from "jsonwebtoken";
+export const sendcookies = (user, res, message) => {
+    const token = jwt.sign({ _id: user._id }, process.env.jwt_secret);
+    res.cookie("token", token, {
         httpOnly: true,
-        maxAge:15*60*1000,
-        sameSite:process.env.Node_ENV== "Development"? "lax":"none",
-        secure:process.env.Node_ENV== "Development"? false: true
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+        sameSite: process.env.Node_ENV == "Development" ? "lax" : "none",
+        secure: process.env.Node_ENV == "Development" ? false : true
     }).json({
         success: true,
         message: message
-    })
-}
+    });
+};
